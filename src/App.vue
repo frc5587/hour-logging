fe3d7c11935656c998a3d2588c681f8a8e912387
Brendan Doney <bre.doney@gmail.com>
2019-05-31 17:03:02 -0400

    Organise people by `checkedIn` and use dummy data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,72 @@
 <template>
   <div id="app">
     <TheHeader></TheHeader>
-    <PersonInfo :id="1" name="Jane Doe" :hours="3" :minutes="23" :checkedIn="false"></PersonInfo>
+
+    <SectionLabel v-if="peopleCheckedIn.length > 0" text="IN"></SectionLabel>
+    <PersonInfo
+      v-for="person in peopleCheckedIn"
+      :key="person.id"
+      :id="person.id"
+      :name="person.name"
+      :hours="person.hours"
+      :minutes="person.minutes"
+      :checkedIn="person.checkedIn"
+    ></PersonInfo>
+
+    <SectionLabel v-if="peopleCheckedOut.length > 0" text="OUT"></SectionLabel>
+    <PersonInfo
+      v-for="person in peopleCheckedOut"
+      :key="person.id"
+      :id="person.id"
+      :name="person.name"
+      :hours="person.hours"
+      :minutes="person.minutes"
+      :checkedIn="person.checkedIn"
+    ></PersonInfo>
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader";
+import SectionLabel from "./components/SectionLabel";
 import PersonInfo from "./components/PersonInfo";
 
 export default {
   name: "app",
   components: {
     TheHeader,
+    SectionLabel,
     PersonInfo
+  },
+  data() {
+    return {
+      people: [
+        {
+          id: 1,
+          name: "Jane Doe",
+          hours: 3,
+          minutes: 23,
+          checkedIn: false,
+          checkInDate: null
+        },
+        {
+          id: 2,
+          name: "John Doe",
+          hours: 19,
+          minutes: 57,
+          checkedIn: true,
+          checkInDate: Date()
+        }
+      ]
+    };
+  },
+  computed: {
+    peopleCheckedIn() {
+      return this.people.filter(person => person.checkedIn);
+    },
+    peopleCheckedOut() {
+      return this.people.filter(person => !person.checkedIn);
+    }
   }
 };
 </script>
---
+++ src/components/SectionLabel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionLabel",
+  props: {
+    text: String
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  /* Text styling */
+  color: #787878;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: normal;
+  font-size: 1em;
+
+  /* The width/alignment is based on the grid of PersonInfo */
+  width: 80px;
+  margin-top: 20px;
+  margin-bottom: 5px;
+}
+</style>
