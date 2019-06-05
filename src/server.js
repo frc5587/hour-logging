2fb47923f173943ad3362c497dc0a03d45f5b483
Brendan Doney <bre.doney@gmail.com>
2019-06-05 14:29:50 -0400

    Complete server script for fetching student data

--- src/server.js
+++ src/server.js
@@ -1,7 +1,35 @@
+/* eslint-disable no-console */
 const express = require("express");
+const mysql = require('mysql');
 const fs = require('fs');
 
 const app = express();
 
-const obj = JSON.parse(fs.readFileSync(`${__dirname}/credentials.json`, 'utf8'));
-console.log(obj);
+const config = JSON.parse(fs.readFileSync(`${__dirname}/credentials.json`, 'utf8'));
+const database = mysql.createConnection({
+  host: 'localhost',
+  user: config.username,
+  password: config.password,
+  database: config.database,
+});
+
+database.connect((err) => {
+  if (err) {
+    throw new Error(`Unable to connect to MySQL with the following credentials: \n${JSON.stringify(config)}`);
+  }
+  console.log(`Connected to table ${config.table} on database ${config.database}!`);
+});
+
+app.get('/students', (_req, res) => {
+  const query = `SELECT student_id, student_name, minutes FROM ${config.table}`;
+  database.query(query, (err, queryResult) => {
+    if (err) {
+      throw new Error(`Error in executing query '${query}'`);
+    }
+    res.send(queryResult);
+  });
+});
+
+app.listen(config.port, () => {
+  console.log(`Hour logging server listening on port ${config.port}...`);
+});
